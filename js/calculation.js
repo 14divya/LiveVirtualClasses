81e1a13bafaec859de6b706b62cfcfdd6c7e6f96
Divya Bhat <divbhat@publicisgroupe.net>
2023-09-13 15:36:10 +0530

    updates

--- js/calculation.js
+++ js/calculation.js
@@ -1,21 +1,26 @@
-function calculate(){
-    var m = document.getElementById("m").value;
-    var cs = document.getElementById("cs").value;
-    var s = document.getElementById("s").value;
+function calculate() {
+  var name = document.getElementById("name").value;
+  var m = document.getElementById("m").value;
+  var cs = document.getElementById("cs").value;
+  var s = document.getElementById("s").value;
+  if (name === "") {
+    alert("Please enter a name");
+  } else if (m === "" || cs === "" || s === "") {
+    alert("Hey " + name + "! Please enter all the marks");
+  } else {
     var tm = document.getElementById("tm");
     var per = document.getElementById("per");
     var max = document.getElementById("max");
-    var total = eval(m)+eval(cs)+eval(s);
+    var total = eval(m) + eval(cs) + eval(s);
     tm.value = total;
-    var percentage = total/3;
-    per.value = percentage+" %";
-    if(eval(m) > eval(cs) && eval(m) > eval(s)){
-        max.value = m;
+    var percentage = total / 3;
+    per.value = percentage + " %";
+    if (eval(m) > eval(cs) && eval(m) > eval(s)) {
+      max.value = m;
+    } else if (eval(cs) > eval(s)) {
+      max.value = cs;
+    } else {
+      max.value = s;
     }
-    else if(eval(cs) > eval(s)){
-        max.value = cs;
-    }
-    else{
-        max.value = s;
-    }
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,82 +1,105 @@
 function jfun() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var suggestions = document.getElementById("suggestions").value;
-    if (name == "" || email == "" || suggestions == "") {
-        alert("Enter the fields");
-    }
-    else {
-        alert("Thanks for contacting with us for LVC Courses !");
-    }
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var suggestions = document.getElementById("suggestions").value;
+  if (name == "" || email == "" || suggestions == "") {
+    alert("Enter the fields");
+  } else {
+    alert("Thanks for contacting with us for LVC Courses !");
+  }
 }
 function subscribe() {
-    var name = prompt("Enter your name");
-    console.log(name);
-    var country;
-    if (name != null) {
-        country = prompt("Enter your country");
-        var discount = "none";
-        if (country == "India") {
-            discount = "50%";
-            alert(name + ", you are successfully subscribed with a discount of " + discount);
-        } else {
-            alert(name + ", you are successfully subscribed with no discount :(");
-        }
-
+  var name = prompt("Enter your name");
+  console.log(name);
+  var country;
+  if (name !== "" && name !== null) {
+    country = prompt("Enter your country");
+    if(country !== null){
+    var discount = "none";
+    if (country === "India") {
+      discount = "50%";
+      alert(
+        name +
+          ", you are successfully subscribed with a discount of " +
+          discount
+      );
     } else {
-        alert("Subscription unsuccesful!!!")
+      alert(name + ", you are successfully subscribed without any discount");
     }
-
+}else{
+    alert("Subscription unsuccessful!!!")
+}
+  } else {
+    alert("Subscription unsuccesful!!!");
+  }
 }
 function openwin() {
-    window.open("instruction.html", "Instructions", "width=800,height=800,left=250");
+  window.open(
+    "instruction.html",
+    "Instructions",
+    "width=800,height=800,left=250"
+  );
 }
 function openDetails() {
-
-    var course = document.getElementById("dropdown").value;
-    if (course == "mern") {
-        var myWindow = window.open("mern.html", "MERN", "width=500,height=500,left=350,top=100");
-
-    } else if (course == "mean") {
-        var myWindow = window.open("mean.html", "MEAN", "width=500,height=500,left=350,top=100");
-
-    } else {
-        var myWindow = window.open("mevn.html", "MEVN", "width=500,height=500,left=350,top=100");
-    }
-
+  var course = document.getElementById("dropdown").value;
+  if (course == "mern") {
+    window.open("mern.html", "MERN", "width=500,height=500,left=350,top=100");
+  } else if (course == "mean") {
+    window.open("mean.html", "MEAN", "width=500,height=500,left=350,top=100");
+  } else {
+    window.open("mevn.html", "MEVN", "width=500,height=500,left=350,top=100");
+  }
 }
 function openTest() {
-    window.open("test.html");
+  window.open("test.html");
 }
 function checkResult() {
-
-    var ans1 = document.getElementsByName("q1");
-    var ans2 = document.getElementsByName("q2");
-    var ans3 = document.getElementsByName("q3");
-    var ans4 = document.getElementsByName("q4");
-    var ans5 = document.getElementsByName("q5");
-    var tm1 = 0;
-    var tm2 = 0;
-    var tm3 = 0;
-    var tm4 = 0;
-    var tm5 = 0;
-    var tm = 0;
-    if (ans1[1].checked == true) {
-        tm1 = 2;
+  var ans1 = document.getElementsByName("q1");
+  var ans2 = document.getElementsByName("q2");
+  var ans3 = document.getElementsByName("q3");
+  var ans4 = document.getElementsByName("q4");
+  var ans5 = document.getElementsByName("q5");
+  var tm = 0;
+  if (ans1[1].checked == true) {
+    tm += 2;
+  }
+  if (ans2[2].checked == true) {
+    tm += 2;
+  }
+  if (ans3[0].checked == true) {
+    tm += 2;
+  }
+  if (ans4[1].checked == true) {
+    tm += 2;
+  }
+  if (ans5[2].checked == true) {
+    tm += 2;
+  }
+  alert("You scored " + tm + " marks!");
+}
+function countDown() {
+  var timex = document.getElementById("demo");
+  timex.classList.remove('btn-info');
+  timex.classList.add('btn-warning');
+  var j = 12;
+  var time = setInterval(function () {
+    timex.innerHTML = (j-2) + " secs";
+    j--;
+   
+    if(j == 0){
+        clearInterval(time);
+        alert("Time's up!!!");
+        checkResult();
+        timex.innerHTML = "Start timer";
+        timex.classList.remove('btn-warning');
+  timex.classList.add('btn-info');
     }
-    if (ans2[2].checked == true) {
-        tm2 = 2;
-    }
-    if (ans3[0].checked == true) {
-        tm3 = 2;
-    }
-    if (ans4[1].checked == true) {
-        tm4 = 2;
-    }
-    if (ans5[2].checked == true) {
-        tm5 = 2;
-    }
-    tm = tm1 + tm2 + tm3 + tm4 + tm5;
-    alert("You scored " + tm + " marks!");
-
+  }, 1000);
+// for(i=4,j=60;j>=0;j--){
+//     timex.innerHTML = i + " mins : " + j + " secs";
+//     if(j == 0){
+//         i--;
+//         j=60;
+//     }
+// }
 }
